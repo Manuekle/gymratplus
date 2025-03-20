e148702a0a6d21be0bb299457f34a15c32a678b7
manudev <meerazo7@hotmail.com>
2025-03-19 21:24:37 -0500

    refactor: disable ESLint rule for unused variables in exercise progress route

--- src/app/api/exercise-progress/[id]/route.ts
+++ src/app/api/exercise-progress/[id]/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
 import { prisma } from "@/lib/prisma";
