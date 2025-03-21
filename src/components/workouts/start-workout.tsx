074728d442f448d08303e7b4aa32791c4254d510
manudev <meerazo7@hotmail.com>
2025-03-20 23:46:36 -0500

    alert

--- src/components/workouts/start-workout.tsx
+++ src/components/workouts/start-workout.tsx
@@ -20,7 +20,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { toast } from "sonner";
 import { AlertCircleIcon } from "hugeicons-react";
 
