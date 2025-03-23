fc8cbcba2ba95b46fc42119f7a524a4085f257af
manudev <meerazo7@hotmail.com>
2025-03-23 16:22:34 -0500

    solucion

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: "https://gymratplus.vercel.app", // 🔴 Reemplaza con tu dominio
+  generateRobotsTxt: true, // Genera automáticamente robots.txt
+  sitemapSize: 5000, // Cantidad de URLs por sitemap
+};
+
+module.exports = config;
---
+++ typings/next-sitemap.config.d.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/** @type {import('next-sitemap').IConfig} */
+declare interface configType {
+  siteUrl: any;
+}
