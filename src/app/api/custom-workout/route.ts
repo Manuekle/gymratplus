67545cea1a293a9a8714bb9a3fdf49518260f802
manudev <meerazo7@hotmail.com>
2025-03-24 18:39:29 -0500

    ahora si?

--- src/app/api/custom-workout/route.ts
+++ src/app/api/custom-workout/route.ts
@@ -204,7 +204,7 @@ function formatWorkoutPlan(
     return acc;
   }, {});
 
-  // Format each muscle group
+  // Format each muscle groups
   return Object.entries(exercisesByDay).map(([muscleGroup, exercises]) => {
     return {
       day: muscleGroup, // Use muscle group as day identifier
