e738ecf70730f598be3e8e0aab0266c5b1fd3537
Manuel Esteban Erazo Medina <meerazo7@hotmail.com>
2025-12-22 22:16:04 -0500

    auth

--- src/app/api/instructors/workouts/assigned/[id]/route.ts
+++ src/app/api/instructors/workouts/assigned/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/database/prisma";
-import { auth } from "../@auth";
+import { auth } from "@auth";
 
 export async function GET(
   request: NextRequest,
--- src/app/api/workout-streak/check-reset/route.ts
+++ src/app/api/workout-streak/check-reset/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse, NextRequest } from "next/server";
 import { WorkoutStreakService } from "@/lib/workout/workout-streak-service";
-import { auth } from "../../../../../auth.ts";
+import { auth } from "@auth";
 
 /**
  * Endpoint para verificar y resetear rachas
