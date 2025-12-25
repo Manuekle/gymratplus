62e38a605379878cc9f2a95710379ea7c79cc46a
Manuel Esteban Erazo Medina <meerazo7@hotmail.com>
2025-12-25 18:27:36 -0500

    feat: install Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,10 +74,33 @@ export default function RootLayout({
       className={`${GeistSans.variable} ${GeistMono.variable}`}
       suppressHydrationWarning
     >
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KSV33ZCJ');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body
         className="min-h-screen bg-background antialiased"
         suppressHydrationWarning
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KSV33ZCJ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <JsonLd />
         <ClientLayout>{children}</ClientLayout>
         <SpeedInsights />
